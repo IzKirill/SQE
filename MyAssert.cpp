cd6f0504975413701094661118011fd8704e7d2f
IzKirill <izmailov.ka@phystech.edu>
2023-08-25 17:54:45 +0500

    Add func to my Assert

--- MyAssert.cpp
+++ MyAssert.cpp
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void MyAssert(int expression, const int nline, const char file[])
+void MyAssert(int expression, const int nline, const char file[], const char func[])
 {
     if(expression == 0)
     {
-        printf("Error(: Line - %d, File(%s)\n", nline, file);
+        printf("Error(: Line - %d, File(%s), Function - %s\n", nline, file, func);
         exit(5);
     }
 }

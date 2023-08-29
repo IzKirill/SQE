a9a1092ff4ec3876c61a27f11fbddca7957602d1
IzKirill <izmailov.ka@phystech.edu>
2023-08-29 16:06:21 +0300

    del struct

--- SQEtest.h
+++ SQEtest.h
@@ -1,13 +1,6 @@
 #ifndef __SQETEST_H__
 #define __SQETEST_H__
 
-struct TestData
-{
-    double a, b, c;
-    double x1, x2;
-    Solutions nroots;
-};
-
 int SQEtest(int* n_of_tests, char file[]);
 
 void sort_roots (double* x1, double* x2);

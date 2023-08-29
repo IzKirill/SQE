f7a53c4fbf40fc0b68f21f609ac2fec971690719
IzKirill <izmailov.ka@phystech.edu>
2023-08-29 14:08:59 +0300

    small fix inter

--- SQEcommand.cpp
+++ SQEcommand.cpp
@@ -1,53 +1,14 @@
 #include "SQEsolver.h"
 #include "SQEtest.h"
+#include "SQEcommand.h"
 #include "MyAssert.h"
 #include "SQEchoice.h"
 #include "SQEstdio.h"
-#include "SQEcommand.h"
 #include <stdio.h>
 #include <math.h>
 #include <windows.h>
 
 HANDLE console_command = GetStdHandle(STD_OUTPUT_HANDLE);
-
-int main_menu()
-{
-    SetConsoleTextAttribute(console_command, (0 << 4) | 14);
-    printf("\t\t" "Hello, enter the number:\n");
-
-    SetConsoleTextAttribute(console_command, (0 << 4) | 1);
-    printf("1: Solve quadratic equation.\t");
-
-    SetConsoleTextAttribute(console_command, (0 << 4) | 5);
-    printf("2: test code.\t");
-
-    SetConsoleTextAttribute(console_command, (0 << 4) | 4);
-    printf("3: quit" "\n");
-
-    SetConsoleTextAttribute(console_command, (0 << 4) | 15);
-
-    int your_choice = 0;
-    User_Choice(&your_choice);
-
-    GAssert(your_choice == 1 || your_choice == 2 ||
-                your_choice == 3);
-
-    if (your_choice == 2)
-    {
-        char default_file[] = "Tests.csv";
-        test(default_file);
-    }
-
-    else if (your_choice == 1)
-    {
-        square();
-    }
-
-    else if (your_choice == 3)
-    {
-        return 0;
-    }
-}
 
 void test(char file[])
 {
@@ -55,7 +16,7 @@ void test(char file[])
     int nOK = SQEtest(&n_of_tests, file);
 
     SetConsoleTextAttribute(console_command, (0 << 4) | 2);
-    printf("%d/%d successfully completed tests.", nOK, n_of_tests);
+    printf("[%d/%d] successfully completed tests.", nOK, n_of_tests); // [10/5]
 
     SetConsoleTextAttribute(console_command, (0 << 4) | 15);
 }
@@ -98,11 +59,49 @@ void linear()
     output_solveQE(nSolutions, x, 0);
 }
 
+int main_menu()                 // linear добавить
+{
+    SetConsoleTextAttribute(console_command, (0 << 4) | 14);
+    printf("\t\t" "Hello, enter the number:\n");
+
+    SetConsoleTextAttribute(console_command, (0 << 4) | 1);
+    printf("1: Solve quadratic equation.\t");
+
+    SetConsoleTextAttribute(console_command, (0 << 4) | 5);
+    printf("2: test code.\t");
+
+    SetConsoleTextAttribute(console_command, (0 << 4) | 4);
+    printf("3: quit" "\n");
+
+    SetConsoleTextAttribute(console_command, (0 << 4) | 15);
+
+    int your_choice = 0;
+    user_choice(&your_choice);           // возвр значение
+
+    GAssert(your_choice == 1 || your_choice == 2 ||
+                your_choice == 3);                      // разделить
+
+    if (your_choice == 2)
+    {
+        test("Tests.csv");
+    }
+
+    else if (your_choice == 1)                // switch
+    {
+        square();
+    }
+
+    else if (your_choice == 3)
+    {
+        return 0;
+    }
+}
+
 void help_menu()
 {
-    printf("-help \t\t     Gives the necessary information.\n");
-    printf("-linear \t     Solve linear equation.\n");
-    printf("-square \t     Solve square equation.\n");
-    printf("-test \t\t     Testing solver square equation.\n");
-    printf("-file name_of_file   Testing solver square equation with your tests.\n");
+    printf("--help \t\t     Gives the necessary information.\n");
+    printf("--linear \t     Solve linear equation.\n");
+    printf("--square \t     Solve square equation.\n");
+    printf("--test \t\t     Testing solver square equation.\n");
+    printf("--file name_of_file   Testing solver square equation with your tests.\n");
 }
